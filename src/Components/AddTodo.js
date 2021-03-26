87f9774708c288cf71d1aecd7d3b00ac4076b24f
Ernest Irabor <irabeny89@gmail.com>
2021-03-26 01:50:50 +0100

    add note component ok

---
+++ src/Components/AddTodo.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import { BiAddToQueue, BiArrowBack } from 'react-icons/bi'
+
+const AddTodo = ({ inputData, onInputChange, handleSave }) =>
+  <div className="container">
+    <h3 className="d-flex justify-content-between">
+      <div>Add Todo</div>
+      <Link href="/">
+        <div className="btn-lg btn-outline-secondary"><BiArrowBack /> back</div>
+      </Link>
+    </h3><hr />
+    <textarea className="form-control" value={inputData}
+      onChange={onInputChange} /><br />
+    <button className="btn-lg btn-block btn-primary" 
+      onClick={handleSave} disabled={`${inputData}` ? false : true} 
+    ><BiAddToQueue /> Add</button>
+  </div>
+
+export default AddTodo
