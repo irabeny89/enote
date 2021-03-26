5c7dae73a3125b67b8731bfc8d01f3f040583703
Ernest Irabor <irabeny89@gmail.com>
2021-03-26 15:49:24 +0100

    ✨ edit view page initial commit

---
+++ pages/edit-view.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+function EditViewPage () {
+  return (
+    <>
+      <Head><title>Edit/View | Todo Next</title></Head>
+      edit view page
+    </>
+  )
+}
+
+export default EditViewPage
