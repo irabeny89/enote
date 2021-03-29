b10cf53738956a2831260ed589a78d0b1102dfa8
Ernest Irabor <irabeny89@gmail.com>
2021-03-29 20:40:27 +0100

    🎨 improve structure of code

--- components/Add.js
+++ components/Add.js
@@ -1,18 +1,20 @@
 import Link from 'next/link'
 import { BiAddToQueue, BiArrowBack } from 'react-icons/bi'
 
-const Add = ({ inputData, onInputChange, save }) =>
+const Add = ({ inputData, onInputChange, handleSave }) =>
   <div className="container">
     <h3 className="d-flex justify-content-between">
       <div>Add Todo</div>
       <Link href="/">
-        <button className="btn-lg btn-outline-secondary"><BiArrowBack /> back</button>
+        <button className="btn-lg btn-outline-secondary">
+          <BiArrowBack /> back
+        </button>
       </Link>
     </h3><hr />
     <textarea className="form-control" value={inputData}
       onChange={onInputChange} /><br />
     <button className="btn-lg btn-block btn-primary"
-      onClick={save({ todo: inputData, done: false })}
+      onClick={handleSave}
       disabled={`${inputData}` ? false : true}
     ><BiAddToQueue /> Add</button>
   </div>
