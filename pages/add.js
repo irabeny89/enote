ba2faaa987d6a416bcd04b5010ce8eb677c679ef
Ernest Irabor <irabeny89@gmail.com>
2021-03-29 20:44:09 +0100

    code improvement

--- pages/add.js
+++ pages/add.js
@@ -1,16 +1,21 @@
 import Head from 'next/head'
+import Router from 'next/router';
 import Add from "../components/Add";
+import { addSaveTodo } from '../controllers/crud';
 import { useTxtArea } from '../hooks/hook.inputs'
-import { useSave } from '../hooks/hook.local-storage'
 
 export default function AddPage() {
+  const { inputData, onInputChange } = useTxtArea()
+  const inputState = { inputData, onInputChange }
+  const handleSave = () => 
+    (addSaveTodo({ todo: inputData, done: false }), Router.push('/'))
   return (
     <>
       <Head>
         <title>Add | Next Todo</title>
       </Head>
       <main>
-        <Add {...useTxtArea()} save={useSave} />
+        <Add {...inputState} handleSave={handleSave} />
       </main>
     </>
   )
