3de51232d7959bbed8c1ca06f3872bb3d24aef46
Ernest Irabor <irabeny89@gmail.com>
2021-03-29 20:39:57 +0100

    🔥 remove redundant files

--- hooks/hook.local-storage.js
+++
@@ -1,4 +0,0 @@
-import Router from 'next/router'
-import { save } from '../controllers/crud'
-
-export const useSave = input => () => (save(input), Router.push('/'))
--- pages/api/members.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({
-    members: [{ name: "Ernest Irabor", id: 1 }, { name: "Kazeem", id: 2 }]
-  })
-}
