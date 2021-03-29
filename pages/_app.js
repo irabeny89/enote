ce51e91960cab68b47eb3b66c2e34b000588953a
Ernest Irabor <irabeny89@gmail.com>
2021-03-29 09:03:41 +0100

    code refactor

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Layout from '../src/Components/Layout'
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
