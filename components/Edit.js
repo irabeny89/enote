fdf4ac3ade9d7420c25bd7fe6f6a714e231553b9
Ernest Irabor <irabeny89@gmail.com>
2021-03-29 20:40:58 +0100

    Edit component

---
+++ components/Edit.js
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import { BiArrowBack } from 'react-icons/bi'
+import { GrDocumentUpdate } from 'react-icons/gr'
+
+const Edit = ({ inputData, onInputChange, handleSave }) => 
+  <div className="container">
+    <h3 className="d-flex justify-content-between">
+      <div>Edit Todo</div>
+      <Link href="/">
+        <button className="btn-lg btn-outline-secondary">
+          <BiArrowBack /> back
+        </button>
+      </Link>
+    </h3><hr />
+    <textarea className="form-control" value={inputData}
+      onChange={onInputChange} /><br />
+    <button className="btn-lg btn-block btn-primary"
+      onClick={handleSave} disabled={`${inputData}` ? false : true}
+    ><GrDocumentUpdate /> Save</button>
+  </div>
+
+export default Edit
