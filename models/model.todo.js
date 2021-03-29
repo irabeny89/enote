e0c34bbc8001890d739fa1e5aad14395c3302c80
Ernest Irabor <irabeny89@gmail.com>
2021-03-29 02:24:46 +0100

    🎨 todo data validation

--- models/model.todo.js
+++ models/model.todo.js
@@ -4,13 +4,19 @@ export class Todo {
   id = randomBytes(8).toString('hex')
   createdAt = new Date().toLocaleString()
   constructor(todoObj) {
-    this.todo = todoObj.todo
-    this.done = Todo.validate(todoObj.done)
+    this.todo = Todo.validateTodo(todoObj.todo)
+    this.done = Todo.validateDone(todoObj.done)
   }
-  static validate(prop) {
-    const isBool = typeof prop === 'boolean' ? true : false
-    if (isBool) {
-      return prop
-    } else throw new Error("'done' property should be a Boolean")
+  static validateTodo(prop) {
+    try {
+      if (typeof prop === 'string' && prop !== '') return prop
+      else throw new Error("Invalid entry")
+    } catch (e) { throw new Error(e.message) }
   }  
+  static validateDone(prop) {
+    try {
+      if (typeof prop === 'boolean') return prop
+      else throw new Error("Invalid entry")
+    } catch (e) { throw new Error(e.message) }
+  }
 }
