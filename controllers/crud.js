3ef6c689f56b78f49d4eaee2ba51e496961a9bc7
Ernest Irabor <irabeny89@gmail.com>
2021-03-29 14:29:26 +0100

    🎨 improvement and refactor

--- controllers/crud.js
+++ controllers/crud.js
@@ -1,16 +1,38 @@
 import { Todo } from '../models/model.todo'
 
+const sortByDoneSave = todoList => {
+  try {
+    const doneTodos = todoList.filter(todoItem => todoItem.done === true)
+    const undoneTodos = todoList.filter(todoItem => todoItem.done === false)
+    const sortedTodos = [...undoneTodos, ...doneTodos]
+    saveTodoToLocalStorage(sortedTodos)
+  } catch (e) { throw new Error(e.message) }
+}
+const saveTodoToLocalStorage = data => localStorage.setItem("todos", JSON.stringify(data))
+
 export const getTodos = () => {
   try {
     const lsData = JSON.parse(localStorage.getItem("todos"))
     return [...(lsData ? lsData : [])]
   } catch (e) { throw new Error(e) }
 }
-
-export const save = whatTodo => {
+export const saveTodos = (...todoItems) => {
+  try {
+    todoItems.length > 1 
+      ? sortByDoneSave(todoItems) 
+      : saveTodoToLocalStorage([...todoItems, ...getTodos()])
+  } catch (e) { throw new Error(e) }
+}
+export const removeTodo = id => {
+  try {
+    const filteredList = getTodos().filter(item => item.id !== id)
+    localStorage.removeItem("todos")
+    saveTodos(...filteredList)
+  } catch (e) { throw new Error(e.message) }
+}
+export const addSaveTodo = whatTodo => {
   try {
     const todo = new Todo(whatTodo)
-    const todos = JSON.stringify([todo, ...getTodos()])
-    localStorage.setItem("todos", todos)
+    saveTodos(todo)
   } catch (e) { throw new Error(e) }
 }
