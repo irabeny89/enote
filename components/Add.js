cd95bb5335760a69ca780a6fe42df5abbf240213
Ernest Irabor <irabeny89@gmail.com>
2021-03-29 09:02:19 +0100

    Add component

---
+++ components/Add.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import { BiAddToQueue, BiArrowBack } from 'react-icons/bi'
+
+const Add = ({ inputData, onInputChange, save }) =>
+  <div className="container">
+    <h3 className="d-flex justify-content-between">
+      <div>Add Todo</div>
+      <Link href="/">
+        <button className="btn-lg btn-outline-secondary"><BiArrowBack /> back</button>
+      </Link>
+    </h3><hr />
+    <textarea className="form-control" value={inputData}
+      onChange={onInputChange} /><br />
+    <button className="btn-lg btn-block btn-primary"
+      onClick={save({ todo: inputData, done: false })}
+      disabled={`${inputData}` ? false : true}
+    ><BiAddToQueue /> Add</button>
+  </div>
+
+export default Add
